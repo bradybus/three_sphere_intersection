f466b2a70d8e3192811c55887b79ae16b8f1b9b1
bradybus <brady.bus@gmail.com>
2021-01-23 01:08:02 -0700

    general enhancements, corrected c eq, py3

--- three_sphere_intersection.py
+++ three_sphere_intersection.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 """
 Showcases the answer of a stackoverflow question:
 https://stackoverflow.com/a/1407395/177931
@@ -22,26 +22,10 @@ def get_spanning_vectors_of_3d_plane(n):
     n = np.asarray(n)
     n = n / np.linalg.norm(n)
 
-    # Generate u
-    while True:
-        u = np.random.randn(3)
-        # Must be careful not to generate a co-linear vector
-        if np.abs(np.dot(u, n)) < 1e-6:
-            continue  # retry
-        u = u - np.dot(u, n) * n
-        u = u / np.linalg.norm(u)
-        break
-
-    # Generate v
-    while True:
-        v = np.random.randn(3)
-        # Must be careful not to generate a co-linear vector
-        if np.abs(np.dot(v, n)) < 1e-6 or np.abs(np.dot(v, u)) < 1e-6:
-            continue  # retry
-        v = v - np.dot(v, u) * u
-        v = v - np.dot(v, n) * n
-        v = v / np.linalg.norm(v)
-        break
+    u = np.array([-n[1], n[0], 0])
+    u = u / np.linalg.norm(u)
+    v = np.cross(n, u)
+    v = v / np.linalg.norm(v)
 
     return u, v
 
@@ -49,14 +33,15 @@ def get_spanning_vectors_of_3d_plane(n):
 def eval_circle(t, circle):
     """
     Evaluates a circle in 3d.
-    :param t: (ndarray) a ndarray of one or more points in 3d in to evaluate circle at.
+    :param t: (ndarray, domain=[0:2*pi)) a ndarray of one or more angles at which the 3d circle will be evaluated.
     :param circle: (circle) the circle in 3d to evaluate.
     :return: (ndarray) a ndarray of the 3d points that each value of t maps to, on on each row.
 
     Note for a description of the circle structure, please see documentation of Sphere.get_circle_of_intersection.
     """
     h, c, u, v = circle
-    return c + h * np.cos(t.reshape(-1, 1)) * u + h * np.sin(t.reshape(-1, 1)) * v
+    circle_pts = c + h * np.cos(t) * u + h * np.sin(t) * v
+    return circle_pts
 
 
 def solve_sin_cos_eq(alpha, beta, gamma):
@@ -69,9 +54,7 @@ def solve_sin_cos_eq(alpha, beta, gamma):
     :param beta: (real)
     :param gamma: (real)
     :return: all solutions for t to the equation.
-
     """
-    sq = np.square
 
     # Solve for tt = tan(t/2) in the original equations using
     # Weierstrass Substitution:
@@ -81,11 +64,14 @@ def solve_sin_cos_eq(alpha, beta, gamma):
     c = gamma - alpha
     b = -2 * beta
     a = gamma + alpha
-    discriminant = sq(b) - 4 * a * c
+    discriminant = b*b - 4*a*c
     if discriminant < 0:
         return np.nan, np.nan
-    tt_0 = (-b + np.sqrt(discriminant)) / (2 * a)
-    tt_1 = (-b - np.sqrt(discriminant)) / (2 * a)
+
+    denom = 1 / (2 * a)
+    discrim_root = np.sqrt(discriminant)
+    tt_0 = (-b + discrim_root) * denom
+    tt_1 = (-b - discrim_root) * denom
 
     t_0 = 2 * np.arctan(tt_0)
     t_1 = 2 * np.arctan(tt_1)
@@ -137,8 +123,10 @@ class Sphere:
         circle = h, c, u, v
         which describes the analytic form
         x(t) = c + h*cos(t)*u + h*sin(t)*v
-        where h is a constant (radius) and c, u, v are all 3d vectors, and x(t) is the 3d
-        circle as a function of time.
+        where:
+        h is a constant (radius),
+        c, u, v are all 3d vectors, and
+        x(t) is the 3d circle as a function of time.
         """
         r0 = other_sphere.radius
         c0 = other_sphere.center
@@ -149,13 +137,12 @@ class Sphere:
             c0, c1 = c1, c0
         n = c1 - c0
         d = np.linalg.norm(n)
-        u, v = get_spanning_vectors_of_3d_plane(n / d)
+        n_normed = n / d
+
+        u, v = get_spanning_vectors_of_3d_plane(n_normed)
+
         x = (r1 * r1 - r0 * r0 + d * d) / (2 * d)
-        if d < r0:
-            x = -x
-            c = c0 + n / d * (d + x)
-        else:
-            c = c0 + n / d * (d - x)
+        c = c0 + n_normed * (d - x)
         h = np.sqrt(r1 * r1 - x * x)
         return h, c, u, v
 
@@ -185,14 +172,16 @@ class Sphere:
         if np.isnan(t0) or np.isnan(t1):
             return None
 
-        return eval_circle(t0, circle)[0], eval_circle(t1, circle)[0]
+        p0 = eval_circle(t0, circle)[0]
+        p1 = eval_circle(t1, circle)[0]
+        return p0, p1
 
 
 # Set a random seed that creates an example where all three spheres intersect (trial-and-error finds one)
 np.random.seed(1012)
 # np.random.seed(1088) # another interesting example
 # Create the three 3d spheres to check for intersection
-print 'Generating 3 random spheres.'
+print('Generating 3 random spheres.')
 spheres = [Sphere() for _ in range(3)]
 # If the first two spheres do not intersect, no point continuing, explain this to the user and quit early.
 if not spheres[0].check_intersection(spheres[1]):
@@ -204,12 +193,12 @@ circle = spheres[0].get_circle_of_intersection(spheres[1])
 ret_value = spheres[2].find_intersection_with_circle(circle)
 if ret_value:
     p0, p1 = ret_value
-    print 'The two 3d points that are in the intersection of the 3 spheres are:'
-    print 'P0 = ', p0
-    print 'P1 = ', p1
-    print 'The following checks to see P0 exists on all three spheres:', all(
-        [s.check_point_is_on_sphere(p0) for s in spheres])
-    print 'The following checks to see P1 exists on all three spheres:', all(
-        [s.check_point_is_on_sphere(p1) for s in spheres])
+    p0_on_all_spheres = all([s.check_point_is_on_sphere(p0) for s in spheres])
+    p1_on_all_spheres = all([s.check_point_is_on_sphere(p1) for s in spheres])
+    print('The two 3d points where all 3 spheres intersect are:')
+    print('P0 = ', p0)
+    print('P1 = ', p1)
+    print(f'P0 exists on all three spheres: {p0_on_all_spheres}')
+    print(f'P1 exists on all three spheres: {p1_on_all_spheres}')
 else:
-    print 'No intersection exists of the circle with the 3rd sphere.'
+    print('No intersection exists for the circle and the 3rd sphere.')
